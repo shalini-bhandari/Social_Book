20038df1307f47a935cc659563470ae37adf9275
Shalini Bhandari <shalini2001bhandari@gmail.com>
2022-05-25 21:43:46 +0530

    Made user Profile, post a picture, LikePost

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from operator import index
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),   #blank means setting up url as home url
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload , name='upload'),
+    path('like-post', views.like_post , name='like-post'),
+    path('signup', views.signup , name = 'signup'),
+    path('signin', views.signin , name='signin'),
+    path('logout', views.logout, name='logout'),
+]
+#views.index will look into views.py for function called index
